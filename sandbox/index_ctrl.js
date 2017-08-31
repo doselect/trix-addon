dfda2b207f907f0d526e9384aa68f8597dc7f82d
saikat-sinha <saikat@myself.com>
2017-08-31 18:40:10 +0530

    Added Superscript and Subscript functionality

--- sandbox/index_ctrl.js
+++ sandbox/index_ctrl.js
@@ -1,6 +1,8 @@
 
 angular.module("trixSandbox", ['angularTrix'])
   .controller('sandboxEditorCtrl', ['$scope', function ($scope) {
-    console.log('test')
+
+    $scope.description = 'this is a test model text 1.sub<sub>2</sub> 2. sup<sup>3</sup> 3. strong <strong>Hello</strong>'
+
   }])
 
--- trix-addon.js
+++ trix-addon.js
@@ -1,1 +1,47 @@
 /* Trix AddOn File */
+(function() {
+  document.addEventListener('trix-initialize', function(e) {
+    trix = e.target;
+    toolBar = trix.toolbarElement;
+    Trix.config.textAttributes.sup = {tagName: 'sup'}
+    Trix.config.textAttributes.sub = {tagName: 'sub'}
+
+    //Superscript
+    superscriptButtonElem = document.createElement('button')
+    superscriptButtonElem.setAttribute('type', 'button')
+    superscriptButtonElem.setAttribute('title', 'Superscript')
+    superscriptButtonElem.setAttribute('tabindex', '-1')
+    superscriptButtonElem.setAttribute('class', 'icon superscript')
+    superscriptButtonElem.innerText = 'Superscript'
+
+    superscriptButton = toolBar.querySelector('.button_group.block_tools').appendChild(superscriptButtonElem)
+
+    superscriptButton.addEventListener('click', function (event) {
+      var range = trix.editor.getSelectedRange()
+      var selectedText = trix.editor.getDocument().getStringAtRange(range)
+      trix.editor.setSelectedRange(range)
+      trix.editor.insertHTML('<sup>' + selectedText + '</sup>')
+    })
+
+
+    // Subscript
+    subscriptButtonElem = document.createElement('button')
+    subscriptButtonElem.setAttribute('type', 'button')
+    subscriptButtonElem.setAttribute('title', 'Subscript')
+    subscriptButtonElem.setAttribute('tabindex', '-1')
+    subscriptButtonElem.setAttribute('class', 'icon subscript')
+    subscriptButtonElem.innerText = 'Subscript'
+
+    subscriptButton = toolBar.querySelector('.button_group.block_tools').appendChild(subscriptButtonElem)
+
+    subscriptButton.addEventListener('click', function (event) {
+      var range = trix.editor.getSelectedRange()
+      var selectedText = trix.editor.getDocument().getStringAtRange(range)
+      trix.editor.setSelectedRange(range)
+      trix.editor.insertHTML('<sub>' + selectedText + '</sub>')
+    })
+
+    // document.querySelector("trix-editor").editor.insertHTML("Hello<sup>2</sup><sub>22</sub>")
+  });
+
+}).call(this);
